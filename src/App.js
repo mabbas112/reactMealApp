51547c23da7d3d6012f513680b4b7343725f3721
mabbas112 <mabbasctn69@gmail.com>
2022-06-03 19:00:04 +0500

    Crating context, useReducer, and forwardRed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 function App() {
    
   const [cartIsShown, setCartIsShown]= useState(false);
@@ -15,16 +16,16 @@ const hideCartHandler = () =>{
 
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {/* Here it means render the code dynamically
        If condition will true then render the component otherwise not*/}
-     {cartIsShown && <Cart onClose={hideCartHandler}/>}
+     {cartIsShown && <Cart sonClose={hideCartHandler}/>}
      {/* onShowCart function name is up to you, but it is moslty followed convention in which we starting with 'on' */}
     <Header onShowCart={showCartHandler}/>
     <main>
     <Meals/>
     </main>
-    </React.Fragment>
+    </CartProvider >
   );
 }
 
