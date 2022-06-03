62f507b7dbbd3ce27f2b717a5bcbf214afda24ca
mabbas112 <mabbasctn69@gmail.com>
2022-06-03 16:34:14 +0500

    adding form, cart functionality with model pop and can by maintaining state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 function App() {
+   
+  const [cartIsShown, setCartIsShown]= useState(false);
+
+const showCartHandler = () =>{
+  setCartIsShown(true);
+}
+const hideCartHandler = () =>{
+  setCartIsShown(false);
+}
+
+
   return (
     <React.Fragment>
-    <Header/>
+      {/* Here it means render the code dynamically
+       If condition will true then render the component otherwise not*/}
+     {cartIsShown && <Cart onClose={hideCartHandler}/>}
+     {/* onShowCart function name is up to you, but it is moslty followed convention in which we starting with 'on' */}
+    <Header onShowCart={showCartHandler}/>
     <main>
     <Meals/>
     </main>
